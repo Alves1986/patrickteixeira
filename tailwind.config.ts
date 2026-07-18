1c4c66bd422000285502cc015746bbd0d6a0a1ea
Alves1986 <cassia.andinho@gmail.com>
2026-07-18 11:52:23 -0300

    fix: set tailwind container to center and add padding

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,19 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1.5rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+      screens: {
+        '2xl': '1280px',
+      },
+    },
     extend: {
       colors: {
         // Pretos / Fundos
